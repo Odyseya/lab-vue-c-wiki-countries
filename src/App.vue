67746415a3b14be5a61d6b6dd11bd25652eb8c01
Anna <ania.pwk.ap@gmail.com>
2024-04-30 21:40:33 +0200

    import fix

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script setup>
-import NavBar from "c:/Users/monts/Desktop/ironhack/vue/lab-vue-c-wiki-countries/src/components/NavBar.vue";
+import NavBar from "/src/components/NavBar.vue";
 </script>
 
 <style scoped></style>
